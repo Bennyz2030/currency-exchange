78cd98c1fb1c6d4b66abd0cde11069f3f7aab0c5
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-22 11:44:10 -0700

    WIP JS UI

--- src/index.js
+++ src/index.js
@@ -4,24 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './exchange.js';
 
-function getRate(response) {
+function getElements(response) {
   let country = $('#country').val();
   if (country === "USD") {
     let USDAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.USD);
-    $('.showMoney').html(` $${USDAmount} `);
+    $('.showMoney').html(`This exchange equates to $${USDAmount} dollars, which is exactly what you already have Charlie! You're in America right now!!`);
   }  else if (country === "AED") {
     let AEDAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.AED);
-    $('.showMoney').html(` ${AEDAmount} dirhams.`);
+    $('.showMoney').html(`This exchange equates to ${AEDAmount} dirhams.`);
   }  else if (country === "AUD") {
     let AUDAmount =Math.round($('#dollarAmount').val() * response.conversion_rates.AUD);
-    $('.showMoney').html(` ${AUDAmount} Australian dollars.`);
+    $('.showMoney').html(`This exchange equates to ${AUDAmount} Australian dollars.`);
   }  else if (country === "EUR") {
     let EURAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.EUR);
-    $('.showMoney').html(` ${EURAmount} euros.`);
+    $('.showMoney').html(`This exchange equates to ${EURAmount} euros.`);
   }  else if (country === "MXN") {
     let MXNAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.MXN);
-    $('.showMoney').html(` ${MXNAmount} pesos.`);
+    $('.showMoney').html(`This exchange equates to ${MXNAmount} pesos.`);
+  }  else if (country === "JOB") {
+    let JOBAmount = $('#dollarAmount').val() * response.conversion_rates.JOB;
+    $('.showMoney').html(`Jobland? You want to go to Jobland, Charlie? Where Jobs grow on Jobbies?! You get ${JOBAmount} dollars, Charlie! That means Not a Number! Nothing! You know why?! Because Jobland doesn't exist, Charlie!!`);
   }  else {
-  $('.showErrors').text(`${response.message}`);
+  $('.showErrors').text(`There was an error: ${response.message}`);
   }
 }
+
+$(document).ready(function() {
+  $('#exchangeMoney').click(function() {
+    Exchange.convertMoney()
+      .then(function(response) {
+        getElements(response);
+      }).catch(function(){
+        ('.showErrors').text('This is an error');
+      });
+  });
+});
