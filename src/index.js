6bd2c92966ec2ae72deaba3dcea621783fd2fa3e
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-22 14:17:27 -0700

    WIP 403 error on API

--- src/index.js
+++ src/index.js
@@ -2,30 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Exchange from './exchange.js';
+import Exchange from './js/exchange.js';
 
-function getElements(response) {
+function getRate(response) {
   let country = $('#country').val();
   if (country === "USD") {
     let USDAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.USD);
-    $('.showMoney').html(`This exchange equates to $${USDAmount} dollars, which is exactly what you already have Charlie! You're in America right now!!`);
+    $('.showMoney').html(` $${USDAmount} `);
   }  else if (country === "AED") {
     let AEDAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.AED);
-    $('.showMoney').html(`This exchange equates to ${AEDAmount} dirhams.`);
+    $('.showMoney').html(` ${AEDAmount} dirhams.`);
   }  else if (country === "AUD") {
     let AUDAmount =Math.round($('#dollarAmount').val() * response.conversion_rates.AUD);
-    $('.showMoney').html(`This exchange equates to ${AUDAmount} Australian dollars.`);
+    $('.showMoney').html(` ${AUDAmount} Australian dollars.`);
   }  else if (country === "EUR") {
     let EURAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.EUR);
-    $('.showMoney').html(`This exchange equates to ${EURAmount} euros.`);
+    $('.showMoney').html(` ${EURAmount} euros.`);
   }  else if (country === "MXN") {
     let MXNAmount = Math.round($('#dollarAmount').val() * response.conversion_rates.MXN);
-    $('.showMoney').html(`This exchange equates to ${MXNAmount} pesos.`);
-  }  else if (country === "JOB") {
-    let JOBAmount = $('#dollarAmount').val() * response.conversion_rates.JOB;
-    $('.showMoney').html(`Jobland? You want to go to Jobland, Charlie? Where Jobs grow on Jobbies?! You get ${JOBAmount} dollars, Charlie! That means Not a Number! Nothing! You know why?! Because Jobland doesn't exist, Charlie!!`);
+    $('.showMoney').html(` ${MXNAmount} pesos.`);
   }  else {
-  $('.showErrors').text(`There was an error: ${response.message}`);
+    $('.showErrors').text(`${response.message}`);
   }
 }
 
@@ -33,9 +30,10 @@ $(document).ready(function() {
   $('#exchangeMoney').click(function() {
     Exchange.convertMoney()
       .then(function(response) {
-        getElements(response);
-      }).catch(function(){
-        ('.showErrors').text('This is an error');
+        getRate(response);
+      })
+      .catch(function() {
+        ('.showErrors').text('Error');
       });
   });
 });
